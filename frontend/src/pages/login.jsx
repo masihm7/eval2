e8ce6577f2493b055650ebda269b1c8074cc462c
Manish <manishmasih786@gmail.com>
2024-10-29 00:37:09 +0530

    fe

--- frontend/src/pages/login.jsx
+++ frontend/src/pages/login.jsx
@@ -1,5 +1,5 @@
 import { useState, useContext } from "react";
-import AuthContext from "../context/AuthContext";
+import AuthContext from "../context/authcontext";
 import { useNavigate } from 'react-router-dom';
 
 export const Login = () => {
